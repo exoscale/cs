f745498891d633a7a31f9213efb3e1da54f94346
Bruno Renié <brutasse@gmail.com>
2016-04-28 08:57:14 +0200

    Bump version to 0.8.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='cs',
-    version='0.8.1',
+    version='0.8.2',
     url='https://github.com/exoscale/cs',
     license='BSD',
     author=u'Bruno Renié',
