e2314746ec41e7ecbd9fc79b175c732ac69672e9
Bruno Renié <brutasse@gmail.com>
2016-11-29 10:23:26 +0100

    Bump version to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='cs',
-    version='0.9.1',
+    version='1.0.0',
     url='https://github.com/exoscale/cs',
     license='BSD',
     author=u'Bruno Renié',
