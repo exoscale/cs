4421a6c7c8ca77b568981e510fb98b4b7feb1c71
Bruno Renié <brutasse@users.noreply.github.com>
2018-01-08 09:55:44 +0100

    project: bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif sys.version_info >= (3, 5):
 
 setup(
     name='cs',
-    version='2.1.0',
+    version='2.1.1',
     url='https://github.com/exoscale/cs',
     license='BSD',
     author='Bruno Renié',
