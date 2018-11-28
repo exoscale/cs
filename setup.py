17532b850851f119234f7dbed0780d3187c15f35
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-11-28 13:32:17 +0100

    project: bump version
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ elif sys.version_info >= (3, 5):
 
 setup(
     name='cs',
-    version='2.4.1',
+    version='2.5',
     url='https://github.com/exoscale/cs',
     license='BSD',
     author='Bruno Renié',
